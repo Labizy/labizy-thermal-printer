65fc645bcb14ef1a9d07e614911d5f2987f9da91
askya_mohamed <m.ceessay@gmail.com>
2021-12-04 17:23:10 +0000

    enable usb print

--- main.js
+++ main.js
@@ -15,11 +15,6 @@ const createWindow = () => {
     win.loadFile('client.html')
 }
 
-
-app.whenReady().then(() => {
-    createWindow()
-})
-
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
